4810b25f5ababd106a1a116a66514fb2b5bd2d79
Alexey Streltsow <socket@sk-macbook.local>
2010-03-04 17:34:45 +0300

    project in xcode created

---
+++ lab/lab.c
@@ -0,0 +1,11 @@
+/*
+ *  lab.c
+ *  lab
+ *
+ *  Created by Alexey Streltsow on 3/4/10.
+ *  Copyright 2010 Zila Networks LLC. All rights reserved.
+ *
+ */
+
+#include "lab.h"
+
---
+++ lab/lab.h
@@ -0,0 +1,22 @@
+/*
+ *  lab.h
+ *  lab
+ *
+ *  Created by Alexey Streltsow on 3/4/10.
+ *  Copyright 2010 Zila Networks LLC. All rights reserved.
+ *
+ */
+
+#include <stdio.h>
+
+// 23.	из четверичной в шестнадцатеричную
+
+
+
+int main (int argc, const char * argv[]) {
+	
+	
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
